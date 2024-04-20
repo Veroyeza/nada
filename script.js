915ad4e70a017eae6c81d55c5d23d92f0d4fa54d
Veronica <166651505+Veroyeza@users.noreply.github.com>
2024-04-20 14:08:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 75;
+let b = 50;
+let c = 66;
+if   ( (c>a) && (c>b) )  {
+    console.log (c)
+}
+if ( (b>a) && (b>c))
+{
+    console.log (b)
+}
+if ( (a>b) && ( a>c))
+{ console.log (a)
+}
